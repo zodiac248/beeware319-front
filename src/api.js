2a969b5641cd67c2fb820ec49e3c1c5f0ef0ef64
zodiac248 <roy971128@gmail.com>
2021-03-18 01:29:49 +0900

    sdfs

--- src/api.js
+++ src/api.js
@@ -4,7 +4,7 @@ import { baseUrl} from "./constants";
 export default class client {
     static async getBuildings() {
         try {
-            const response = axios.get("/building/all");
+            const response = axios.get("https://beeware319.herokuapp.com/building/all");
             return response
         } catch (e) {
             console.log(e.message())
@@ -12,7 +12,7 @@ export default class client {
     }
     static async getBuilding(id) {
         try {
-            const response = axios.get("/building", {params: {id}} );
+            const response = axios.get("https://beeware319.herokuapp.com/building", {params: {id}} );
             return response
         } catch (e) {
             console.log(e.message())
@@ -21,7 +21,7 @@ export default class client {
 
     static async addBuilding({name, address}) {
         try {
-            const response = axios.post("/building", {name, address});
+            const response = axios.post("https://beeware319.herokuapp.com/building", {name, address});
             return response
         } catch (e) {
             console.log(e.message())
@@ -30,7 +30,7 @@ export default class client {
 
     static async updateBuilding({id, name, address}) {
         try {
-            const response = axios.put("/building", {id, name, address});
+            const response = axios.put("https://beeware319.herokuapp.com/building", {id, name, address});
             return response
         } catch (e) {
             console.log(e.message())
@@ -39,7 +39,7 @@ export default class client {
 
     static async deleteBuilding(id) {
         try {
-            const response = axios.delete("/building", id);
+            const response = axios.delete("https://beeware319.herokuapp.com/building", id);
             return response
         } catch (e) {
             console.log(e.message())
@@ -48,7 +48,7 @@ export default class client {
 
     static async getFloors({buildingId}) {
         try {
-            const response = axios.get("/floor", {params: {buildingId}})
+            const response = axios.get("https://beeware319.herokuapp.com/floor", {params: {buildingId}})
             return response
         } catch (e) {
             console.log(e.message())
@@ -57,7 +57,7 @@ export default class client {
 
     static async getFloorInfo(floorID, date) {
         try {
-            const response = axios.get("/floor/info", {params: {id: floorID, date: date}})
+            const response = axios.get("https://beeware319.herokuapp.com/floor/info", {params: {id: floorID, date: date}})
             return response
         } catch (e) {
             console.log(e.message())
@@ -66,7 +66,7 @@ export default class client {
 
     static async addFloor({floorNumber, buildingId, deskIds}) {
         try {
-            const response = axios.post("/floor", {floorNumber, buildingId, deskIds});
+            const response = axios.post("https://beeware319.herokuapp.com/floor", {floorNumber, buildingId, deskIds});
             return response
         } catch (e) {
             console.log(e.message())
@@ -75,7 +75,7 @@ export default class client {
 
     static async updateFloor({id, floorNumber, buildingId}) {
         try {
-            const response = axios.put("/floor", {id, floorNumber, buildingId});
+            const response = axios.put("https://beeware319.herokuapp.com/floor", {id, floorNumber, buildingId});
             return response
         } catch (e) {
             console.log(e.message())
@@ -84,7 +84,7 @@ export default class client {
 
     static async deleteFloor(id) {
         try {
-            const response = axios.delete("/floor", id);
+            const response = axios.delete("https://beeware319.herokuapp.com/floor", id);
             return response
         } catch (e) {
             console.log(e.message())
@@ -93,7 +93,7 @@ export default class client {
 
     static async getDesks(floorID) {
         try {
-            const response = axios.get("/desk", {params: {floorId: floorID}})
+            const response = axios.get("https://beeware319.herokuapp.com/desk", {params: {floorId: floorID}})
             return response
         } catch (e) {
             console.log(e.message())
@@ -102,7 +102,7 @@ export default class client {
 
     static async makeBooking({employeeId, deskId, date}) {
         try {
-            const response = axios.post("/booking", {employeeId, deskId, date});
+            const response = axios.post("https://beeware319.herokuapp.com/booking", {employeeId, deskId, date});
             return response
         } catch (e) {
             console.log(e.message())
@@ -111,7 +111,7 @@ export default class client {
 
     static async updateBooking({id, deskId, employeeId, date}) {
         try {
-            const response = axios.put("/booking", {id, deskId, employeeId, date});
+            const response = axios.put("https://beeware319.herokuapp.com/booking", {id, deskId, employeeId, date});
             return response
         } catch (e) {
             console.log(e.message())
@@ -120,7 +120,7 @@ export default class client {
 
     static async updateDesks({id, deskId, employeeId, date}) {
         try {
-            const response = axios.put("/booking", {id, deskId, employeeId, date});
+            const response = axios.put("https://beeware319.herokuapp.com/booking", {id, deskId, employeeId, date});
             return response
         } catch (e) {
             console.log(e.message())
@@ -129,7 +129,7 @@ export default class client {
 
     static async deleteBooking({id}) {
         try {
-            const response = axios.delete("/booking", {id});
+            const response = axios.delete("https://beeware319.herokuapp.com/booking", {id});
             return response
         } catch (e) {
             console.log(e.message())
@@ -137,7 +137,7 @@ export default class client {
     }
 
     static async getUserInfo() {
-        const response = await axios.get("/user/userinfo", {withCredentials: true})
+        const response = await axios.get("https://beeware319.herokuapp.com/user/userinfo", {withCredentials: true})
         return response
     }
 
