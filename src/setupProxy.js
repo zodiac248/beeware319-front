493ca58b1089c851c45a943c662d8caa5e47ac4c
zodiac248 <roy971128@gmail.com>
2021-03-17 23:33:47 +0900

    sdfs

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,5 +1,5 @@
 const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
-    app.use(proxy('/api', { target: 'https://beeware319.herokuapp.com' }));
+    app.use(createProxyMiddleware('/api', { target: 'https://beeware319.herokuapp.com' }));
 };
