338bdb8ef15896149d18f19be6e1ab6ffa66a43a
zodiac248 <roy971128@gmail.com>
2021-03-24 03:51:00 +0900

    ddddd

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,11 +3,11 @@ const {createProxyMiddleware} = require('http-proxy-middleware');
 
 module.exports = function(app) {
     app.use(createProxyMiddleware('/api', {
-        target: 'https://beeware319.herokuapp.com', //转发到的域名或者ip地址
+        target: 'https://beeware319.herokuapp.com',
         pathRewrite: {
-            '^/api': '', //接口地址里没有"/api",将其重写置空
+            '^/api': '',
         },
-        changeOrigin: true, //必须设置为true
-        secure: false //是否验证https的安全证书，如果域名是https需要配置此项
+        changeOrigin: true,
+        secure: false
     }));
 };
