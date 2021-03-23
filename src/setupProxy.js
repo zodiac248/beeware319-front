0b11705dcd8367a1c4211fc445be0ddf4029fea0
zodiac248 <roy971128@gmail.com>
2021-03-24 03:01:38 +0900

    ddddd

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,12 +1,12 @@
-const {createProxyMiddleware} = require("http-proxy-middleware");
-module.exports = function(app){
+
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
     app.use(
-        createProxyMiddleware("/api",{
-            target:"https://beeware319.herokuapp.com",
-            changeOrigin:true,
-            pathRewrite:{
-                "^/api":""
-            }
+        '/api',
+        createProxyMiddleware({
+            target: 'https://beeware319.herokuapp.com',
+            changeOrigin: true
         })
-    )
-}
+    );
+};
