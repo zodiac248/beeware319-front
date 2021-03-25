ac340e9ab9754f6ece4c07d0d63446628580d04a
zodiac248 <roy971128@gmail.com>
2021-03-26 03:13:22 +0900

    ddddd

--- src/constants.js
+++ src/constants.js
@@ -5,7 +5,7 @@ import ViewPosts from "./components/Social Module/ViewPosts";
 import ViewCategories from "./components/Social Module/ViewTopics";
 import ViewNotifications from "./components/Social Module/ViewNotifications";
 
-export const baseUrl = "https://beeware319.herokuapp.com/"
+export const baseUrl = "https://beeware319.herokuapp.com"
 
 export const roles = {
     admin: "Admin",
